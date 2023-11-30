d460247d799b141b5180a41b474836377fabce97
ElianoDeLaFlor <setekpo@hotmail.fr>
2023-11-30 10:14:26 +0400

    feat(domain): add utility class

---
+++ CleanArchitecture.Domain/Utility/Helper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Domain.Utility
+{
+    public class Helper
+    {
+        public static string Code(int length)
+        {
+            Random r = new();
+            string codeTable = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < length; i++)
+            {
+                int index = r.Next(1, 63);
+                sb.Append(codeTable[index - 1]);
+            }
+            return sb.ToString();
+        }
+    }
+}
