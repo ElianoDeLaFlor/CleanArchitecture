a0ecfd2cd973f54de8a263bb6efbc293678baeb8
ElianoDeLaFlor <setekpo@hotmail.fr>
2023-11-30 10:16:05 +0400

    feat(persistence): add datacontext

---
+++ CleanArchitecture.Persistence/Data/DataContext.cs
@@ -0,0 +1,28 @@
+﻿using CleanArchitecture.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Persistence.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> option) : base(option) { }
+
+        public DbSet<Immobilier> immobiliers { get; set; }
+        public DbSet<ImmobilierTypeImmobilier> immobilierTypeImmobiliers { get; set; }
+        public DbSet<ImmobilierTypeVente> immobilierVente { get; set; }
+        public DbSet<TypeImmobilier> typeImmobiliers { get; set; }
+        public DbSet<TypeVente> typeVentes { get; set; }
+        public DbSet<Utilisateur> Utilisateurs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configure entity mappings, relationships, etc.
+        }
+    }
+}
