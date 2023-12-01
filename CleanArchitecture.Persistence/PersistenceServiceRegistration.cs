4b195bcc092cf9f40949a63f7f0f6149c4e9bfe9
ElianoDeLaFlor <setekpo@hotmail.fr>
2023-12-01 16:12:09 +0400

    feat(immobilier): add registration service

---
+++ CleanArchitecture.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,25 @@
+﻿using CleanArchitecture.Domain.Repositories;
+using CleanArchitecture.Persistence.Data;
+using CleanArchitecture.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceService(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<DataContext>(options=> options.UseSqlServer(configuration.GetConnectionString("")));
+            services.AddScoped<IImmobilierRepository, ImmobilierRepository>();
+            return services;
+        }
+    }
+}
