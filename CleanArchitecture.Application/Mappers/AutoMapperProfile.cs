b65719fada6387fe8aa1f785d7b9204c62e865a0
ElianoDeLaFlor <setekpo@hotmail.fr>
2023-12-01 16:15:32 +0400

    feat(immobilier): application add mapper

---
+++ CleanArchitecture.Application/Mappers/AutoMapperProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using CleanArchitecture.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.Mappers
+{
+    public class AutoMapperProfile:Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Immobilier, ImmobilierDto>();
+        }
+    }
+}
