653eb83615a15a89d984bf097eebeb5710691a39
ElianoDeLaFlor <setekpo@hotmail.fr>
2023-12-06 12:55:02 +0400

    feat(immobilier): project sln files changed

---
+++ CleanArchitecture.Persistence/Dto/EnityMapperProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using CleanArchitecture.Domain.Models;
+using CleanArchitecture.Domain.Utility;
+using CleanArchitecture.Persistence.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Persistence.Dto
+{
+    public class EnityMapperProfile:Profile
+    {
+        public EnityMapperProfile()
+        {
+            CreateMap<Immobilier, ImmobilierEntity>();
+            CreateMap<ImmobilierEntity, Immobilier>();
+        }
+    }
+}
